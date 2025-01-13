4eb43d22447f5c24a801c79618e2bf43cab6cba1
Ali Hamza <therealalihamza@gmail.com>
2025-01-13 20:43:09 +0500

    Update Styling and add javacript

---
+++ Spotify/script.js
@@ -0,0 +1,14 @@
+console.log('Welcome to Spotify');
+
+let audioElement = new Audio('1.mp3');
+let songs = [
+    {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
+    {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
+    {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
+    {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
+    {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
+    {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
+    {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
+    {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
+    {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
+]
