edc32f6baed93801e17f868d14fc95fd1bf3c010
Ali Hamza <therealalihamza@gmail.com>
2025-01-14 03:37:46 +0500

    Update index file and javascript file

--- Spotify/script.js
+++ Spotify/script.js
@@ -1,6 +1,13 @@
 console.log('Welcome to Spotify');
 
+// Initialize the Variables
+let songIndex = 0;
 let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let  myProgressBar = document.getElementById('myProgressBar');
+
+
+
 let songs = [
     {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
     {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
@@ -12,3 +19,14 @@ let songs = [
     {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
     {songName: "KOi kay kehta rahy", filePath: "Spotfify/1.mp3", coverPath: "covers/1.jp"},
 ]
+
+
+// Hnadle Play/Pause Click
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('')
+    }
+})
+// Listen tO Event
+
